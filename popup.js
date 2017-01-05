c2386adf55e9c198289f646a6b4ea6bd12fd3da9
R <rrryu@live.jp>
2017-01-05 10:30:54 +0900

    修正 v0.0.2

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 $(function(){
-  $("#title").on("click", function(){
-    chrome.tabs.getSelected(null, function(tab){
-      $("#text").val("[{0}]({1})".replace("{0}", tab.title).replace("{1}", tab.url));
+  function copyText(val){
+      $("#text").val(val);
       $("#text").select();
       document.execCommand('copy');
+  }
+  $("#Qiita").on("click", function(){
+    chrome.tabs.getSelected(null, function(tab){
+      copyText("[{0}]({1})".replace("{0}", tab.title).replace("{1}", tab.url));
+    });
+  });
+  $("#はてな").on("click", function(){
+    chrome.tabs.getSelected(null, function(tab){
+      copyText("[{1}:title={0}]".replace("{0}", tab.title).replace("{1}", tab.url));
     });
   });
 });
