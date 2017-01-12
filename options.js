008990f86072d0acf9b143098221979339db2116
rrryutaro <rrryutaro@gmail.com>
2017-01-12 15:00:40 +0900

    v0.0.6.1 IMG項目のコピー用の値をエスケープしていなかったミスを修正。

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
-const tagTbl1 = "<tr><td>{0}</td><td>{1}</td></tr>"
-const fsTbl1 = '<input type="image" id="btnCopy" src="copy_16.png" alt="コピー" value="{0}"><input type="image" id="btnRemove" src="trash_16.png" alt="削除" value="{1}">'
+const tag1 = "<tr><td>{0}</td><td>{1}</td></tr>"
+const tag2 = '<input type="image" id="btnCopy" src="copy_16.png" alt="コピー" value="{0}"><input type="image" id="btnRemove" src="trash_16.png" alt="削除" value="{1}">'
 const fs = (fmt, ary)=>{ return formatString(fmt, ary); }
 $(()=>{
   chrome.storage.sync.get(["CopyData"],items=>{
@@ -8,13 +8,13 @@ $(()=>{
       const tbl = $("#copyTable");
       switch (x.Type){
         case "TITLE":
-          tbl.append(fs(tagTbl1, [fs(fsTbl1, [fs("[{0}]({1})", [x.Data.Url, x.Data.Title]), index--]), fs("<a href={0}>{1}</a>", [x.Data.Url, x.Data.Title])]));
+          tbl.append(fs(tag1, [fs(tag2, [fs("[{0}]({1})", [x.Data.Url, x.Data.Title]), index--]), fs("<a href={0}>{1}</a>", [x.Data.Url, x.Data.Title])]));
           break;
       	case "IMG":
-      	  tbl.append(fs(tagTbl1, [fs(fsTbl1, [x.Data, index--]), x.Data]));
+      	  tbl.append(fs(tag1, [fs(tag2, [escapeHtml(x.Data), index--]), x.Data]));
       	  break;
       	default:
-      	  tbl.append(fs(tagTbl1, [fs(fsTbl1, [escapeHtml(x.Data), index--]), fs("<pre>{0}</pre>", [escapeHtml(x.Data)])]));
+      	  tbl.append(fs(tag1, [fs(tag2, [escapeHtml(x.Data), index--]), fs("<pre>{0}</pre>", [escapeHtml(x.Data)])]));
       	  break;
       }
     });
